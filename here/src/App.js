8762bf382709f3e0bec0ebb8b5763046171c7461
william reid gill <blueyeboyreid@gmail.com>
2021-08-30 15:26:56 -0700

    started divs

--- here/src/App.js
+++ here/src/App.js
@@ -3,9 +3,17 @@ import Navbar from "./Components/Navbar";
 
 const App = () => {
   return (
+  <main>
     <div>
       <Navbar/>
     </div>
+    <container>
+      <h2>about me</h2>
+    </container>
+    <container>
+      <h2>Services</h2>
+    </container>
+  </main>
   );
 }
 
