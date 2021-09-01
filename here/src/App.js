5e22c1cf1b6de8be657ceeb1a20ca255dbdc2164
william reid gill <blueyeboyreid@gmail.com>
2021-09-01 14:58:02 -0700

    changed site layout

--- here/src/App.js
+++ here/src/App.js
@@ -10,20 +10,23 @@ const App = () => {
     </div>
       <main className="frosted">
         
-        
     
-    <container className='container about'>
-      <h2 className='title'>About Me</h2>
+    <article className='container'>
+      <h2 className='title' id='about'>About Me</h2>
       <p>Nisi ullamco sit ex exercitation incididunt nulla laborum est excepteur. Lorem labore ad consequat eu est aliqua do elit et tempor dolore nulla voluptate et. Consequat mollit cupidatat voluptate esse qui ex esse est deserunt fugiat ut occaecat officia est.</p>
-    </container>
-    <container className='container services'>
-      <h2 className='title'>Services</h2>
-      <p>Irure do sit aliquip culpa sit. Duis laboris ipsum incididunt officia consequat dolor. Nisi Lorem ad amet aliqua labore sint sunt sunt ad nisi magna.</p>
-    </container>
-    <container className='container contact'>
-      <h2 className='title'>Contact</h2>
+    </article>
+    <article className='container'>
+      <h2 className='title'id='services'>Services</h2>
+      <ul type="">
+  <li>• Mollit dolor sunt et ad adipisicing nisi et ad.</li>
+  <li>• Minim adipisicing laboris ut ad consectetur.</li>
+  <li>• Ad reprehenderit aute incididunt mollit ea qui.</li>
+      </ul>
+    </article>
+    <article className='container'>
+      <h2 className='title'id='contact'>Contact</h2>
       <p>Quis laborum et non non sint aliqua deserunt mollit duis ex eiusmod.Irure do sit aliquip culpa sit.</p>
-    </container>
+    </article>
   </main>
   <Footer/>
   </main>
